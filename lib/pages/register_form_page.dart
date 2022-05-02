831535839163af0fbdc3448734bf8fdea1215e06
Artem <31787423+DokSna@users.noreply.github.com>
2022-05-02 11:13:55 +0300

    48. Стилизация полей ввода

--- lib/pages/register_form_page.dart
+++ lib/pages/register_form_page.dart
@@ -22,27 +22,88 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
           padding: const EdgeInsets.all(16.0),
           children: [
             TextField(
-              decoration: InputDecoration(labelText: 'Full Name *'),
+              decoration: InputDecoration(
+                labelText: 'Full Name *',
+                hintText: 'What do people call you?',
+                prefixIcon: Icon(Icons.person),
+                suffixIcon: Icon(Icons.delete, color: Colors.red),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20.0),
+                  ),
+                  borderSide: BorderSide(color: Colors.black, width: 2.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20.0),
+                  ),
+                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                ),
+              ),
             ),
             SizedBox(height: 10),
             TextFormField(
-              decoration: InputDecoration(labelText: 'Phone Number *'),
+              decoration: InputDecoration(
+                labelText: 'Phone Number *',
+                hintText: 'Where can we rech you?',
+                prefixIcon: Icon(Icons.phone),
+                suffixIcon: Icon(Icons.delete, color: Colors.red),
+                helperText: 'Phone format: (XXX)XXX-XXXX',
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20.0),
+                  ),
+                  borderSide: BorderSide(color: Colors.black, width: 2.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20.0),
+                  ),
+                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                ),
+              ),
             ),
             SizedBox(height: 10),
             TextFormField(
-              decoration: InputDecoration(labelText: 'Email Address'),
+              decoration: InputDecoration(
+                labelText: 'Email Address',
+                hintText: 'Enter a email address',
+                icon: Icon(Icons.email),
+              ),
             ),
             SizedBox(height: 20),
             TextFormField(
-              decoration: InputDecoration(labelText: 'Live Story'),
+              decoration: InputDecoration(
+                labelText: 'Live Story',
+                hintText: 'Tell us about your self',
+                helperText: 'Keep it short, this is just a demo',
+                border: OutlineInputBorder(),
+              ),
+              maxLines: 3,
             ),
             SizedBox(height: 10),
             TextFormField(
-              decoration: InputDecoration(labelText: 'Password'),
+              decoration: InputDecoration(
+                labelText: 'Password',
+                hintText: 'Enter the password',
+                suffixIcon: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.visibility),
+                ),
+                icon: Icon(Icons.security),
+              ),
             ),
             SizedBox(height: 10),
             TextFormField(
-              decoration: InputDecoration(labelText: 'Confirm Password'),
+              decoration: InputDecoration(
+                labelText: 'Confirm Password',
+                hintText: 'Confirm the password',
+                suffixIcon: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.visibility),
+                ),
+                icon: Icon(Icons.border_color),
+              ),
             ),
             SizedBox(height: 15),
             ElevatedButton(
@@ -51,7 +112,6 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
                 'Submit Form',
                 style: TextStyle(color: Colors.white),
               ),
-              // style: style,
               style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.all(Colors.green),
               ),
