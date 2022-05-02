3bb992abaefcb478b4912ddbe1f67d1e0dbcad4c
Artem <31787423+DokSna@users.noreply.github.com>
2022-05-02 19:45:38 +0300

    51. Валидация полей формы

--- lib/pages/register_form_page.dart
+++ lib/pages/register_form_page.dart
@@ -13,6 +13,8 @@ class RegisterFormPage extends StatefulWidget {
 class _RegisterFormPageState extends State<RegisterFormPage> {
   bool _hidePass = true;
 
+  final _formKey = GlobalKey<FormState>();
+
   final _nameController = TextEditingController();
   final _phoneController = TextEditingController();
   final _emailController = TextEditingController();
@@ -40,10 +42,11 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
         centerTitle: true,
       ),
       body: Form(
+        key: _formKey,
         child: ListView(
           padding: const EdgeInsets.all(16.0),
           children: [
-            TextField(
+            TextFormField(
               controller: _nameController,
               decoration: InputDecoration(
                 labelText: 'Full Name *',
@@ -63,6 +66,8 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
                   borderSide: BorderSide(color: Colors.blue, width: 2.0),
                 ),
               ),
+              // validator: (val) => val!.isEmpty ? 'Name is required' : null,
+              validator: _validateName,
             ),
             SizedBox(height: 10),
             TextFormField(
@@ -87,7 +92,14 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
                 ),
               ),
               keyboardType: TextInputType.phone,
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              inputFormatters: [
+                // FilteringTextInputFormatter.digitsOnly,
+                FilteringTextInputFormatter(RegExp(r'^[()\d -]{1,15}$'),
+                    allow: true)
+              ],
+              validator: (value) => _validatePhoneNumber(value!)
+                  ? null
+                  : 'Phone number must be entered as (###)###-####',
             ),
             SizedBox(height: 10),
             TextFormField(
@@ -98,6 +110,7 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
                 icon: Icon(Icons.email),
               ),
               keyboardType: TextInputType.emailAddress,
+              validator: _validateEmail,
             ),
             SizedBox(height: 20),
             TextFormField(
@@ -109,6 +122,7 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
                 border: OutlineInputBorder(),
               ),
               maxLines: 3,
+              inputFormatters: [LengthLimitingTextInputFormatter(100)],
             ),
             SizedBox(height: 10),
             TextFormField(
@@ -129,6 +143,7 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
               ),
               obscureText: _hidePass,
               maxLength: 8,
+              validator: _validatePassword,
             ),
             SizedBox(height: 10),
             TextFormField(
@@ -149,6 +164,7 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
               ),
               obscureText: _hidePass,
               maxLength: 8,
+              validator: _validatePassword,
             ),
             SizedBox(height: 15),
             ElevatedButton(
@@ -168,9 +184,52 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
   }
 
   void _submitForm() {
-    print('name: ${_nameController.text}');
-    print('phone: ${_phoneController.text}');
-    print('email: ${_emailController.text}');
-    print('live story: ${_liveStoryController.text}');
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState?.save();
+      print('Form is valid');
+      print('name: ${_nameController.text}');
+      print('phone: ${_phoneController.text}');
+      print('email: ${_emailController.text}');
+      print('live story: ${_liveStoryController.text}');
+    } else {
+      print('Form is not valid! Please review and correct');
+    }
+  }
+
+  String? _validateName(String? value) {
+    final _nameExp = RegExp(r'^[A-Za-z ]+$');
+    if (value!.isEmpty) {
+      return 'Name is required.';
+    } else if (!_nameExp.hasMatch(value)) {
+      return 'Please enter alphabetical characters.';
+    } else {
+      return null;
+    }
+  }
+
+  bool _validatePhoneNumber(String input) {
+    final _phoneExp = RegExp(r'^\(\d\d\d\)\d\d\d\-\d\d\d\d$');
+
+    return _phoneExp.hasMatch(input);
+  }
+
+  String? _validateEmail(String? value) {
+    if (value!.isEmpty) {
+      return 'Email cannot be empty';
+    } else if (!_emailController.text.contains('@')) {
+      return 'Invalid email address';
+    } else {
+      return null;
+    }
+  }
+
+  String? _validatePassword(String? value) {
+    if (_passController.text.length != 8) {
+      return '8 character required for password';
+    } else if (_confirmPassController.text != _passController.text) {
+      return 'Password does not match';
+    } else {
+      return null;
+    }
   }
 }
