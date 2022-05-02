61343ad6f87a01d51f1dbb4b2b6e4607de8ef912
Artem <31787423+DokSna@users.noreply.github.com>
2022-05-02 10:27:42 +0300

    47. TextField и TextFormField - complete

--- lib/pages/register_form_page.dart
+++ lib/pages/register_form_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class RegisterFormPage extends StatefulWidget {
@@ -18,7 +20,43 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
       body: Form(
         child: ListView(
           padding: const EdgeInsets.all(16.0),
-          children: [],
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Full Name *'),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Phone Number *'),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Email Address'),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Live Story'),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Password'),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Confirm Password'),
+            ),
+            SizedBox(height: 15),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                'Submit Form',
+                style: TextStyle(color: Colors.white),
+              ),
+              // style: style,
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.green),
+              ),
+            ),
+          ],
         ),
       ),
     );
